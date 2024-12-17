ff51d34fa9340f3def96ec9b521e184b0be78abe
Obayouh <80900668+Obayouh@users.noreply.github.com>
2024-12-17 13:39:19 +0100

    Rewrote Respawn script

--- Assets/Assets/Scripts/LevelManager.cs
+++ Assets/Assets/Scripts/LevelManager.cs
@@ -10,72 +10,19 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private float transitionTime = 1f;
 
-    private Vector3 _RespawnPoint;
-    private Coroutine currentCoroutine;
-
     [SerializeField] private GameObject playerCharacter;
-
-    private RespawnPlayer respawnPlayerScript;
 
     private void Start()
     {
-        respawnPlayerScript = FindObjectOfType<RespawnPlayer>();
-        if (respawnPlayerScript != null)
-        {
-            _RespawnPoint = respawnPlayerScript.transform.position;
-        }
-
         if (playerCharacter == null)
         {
             Debug.Log(playerCharacter + " is not found.");
         }
     }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.G) && currentCoroutine == null)
-    //    {
-    //        RestartLife();
-    //        currentCoroutine = StartCoroutine(EPreventSpam());
-
-    //    }
-    //}
-
-    private void RestartLife()
-    {
-        if (CheckpointRegister() == null)
-        {
-            transform.position = _RespawnPoint;
-        }
-    }
-
-    public Vector3 ReturnRespawnPoint()
-    {
-        Debug.Log("not filled in");
-        return _RespawnPoint;
-    }
-
-    public Vector3 CheckpointRegister()
-    {
-        Vector3 currentCheckpointRespawn = new Vector3();
-        Vector3 newCheckpointRespawn = new Vector3();
-
-        if (currentCheckpointRespawn == null)
-        {
-            currentCheckpointRespawn = _RespawnPoint;
-        }
-        else if (currentCheckpointRespawn != null)
-        {
-            currentCheckpointRespawn = newCheckpointRespawn;
-        }
-
-        return currentCheckpointRespawn;
-    }
-    IEnumerator EPreventSpam()
+    private void Update()
     {
 
-        yield return new WaitForSeconds(2f);
-        currentCoroutine = null;
     }
 
     public void LoadNextLevel()
--- Assets/Assets/Scripts/RespawnPlayer.cs
+++ Assets/Assets/Scripts/RespawnPlayer.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class RespawnPlayer : MonoBehaviour
 {
+    //[SerializeField] private LevelManager levelManager;
 
-    [SerializeField] private LevelManager levelManager;
+    [HideInInspector] public Vector3 _PublicRespawnPoint;
+
+    private Vector3 _RespawnPoint;
+    private Vector3 currentCheckpointRespawn;
+
+    private Coroutine currentCoroutine;
 
     void Start()
     {
-        levelManager = FindObjectOfType<LevelManager>();
+        transform.position = _RespawnPoint;
     }
 
     void Update()
@@ -19,9 +25,34 @@ public class RespawnPlayer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("KillPlane"))
+        if (other.gameObject.CompareTag("KillPlane") && currentCoroutine == null)
         {
-            transform.position = levelManager.ReturnRespawnPoint();
+             RestartLife();
+            currentCoroutine = StartCoroutine(EWait());
         }
+
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            currentCheckpointRespawn = other.gameObject.transform.position;
+            _RespawnPoint = currentCheckpointRespawn;
+            Destroy(other.gameObject.GetComponent<BoxCollider>());
+        }
+    }
+
+    public Vector3 ReturnRespawnPoint()
+    {
+        Debug.Log("not filled in");
+        _PublicRespawnPoint = _RespawnPoint;
+        return _PublicRespawnPoint;
+    }
+
+    private void RestartLife()
+    {
+        transform.position = _RespawnPoint;
+    }
+    IEnumerator EWait()
+    {
+        yield return new WaitForSeconds(2f);
+        currentCoroutine = null;
     }
 }
